ba00835a4eb6df658f2976b52902152bce2bafde
LAPTOP-N6NIE006\ACER <dongdiem94@gmail.com>
2020-12-06 20:55:20 +0700

    Add project files.

---
+++ RedisDemo/Controllers/WeatherForecastController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using System;
+
+namespace RedisDemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private IDistributedCache _cache;
+        public WeatherForecastController(IDistributedCache cache)
+        {
+            this._cache = cache;
+        }
+        public string Index()
+        {
+            string test = _cache.GetString("Key-Test") ?? "";
+            if (string.IsNullOrEmpty(test))
+            {
+                _cache.SetString($"Key-Test", $"Value-{DateTime.Now}");
+                test = _cache.GetString("Key-Test") ?? "";
+            }
+            return test;
+        }
+    }
+}
